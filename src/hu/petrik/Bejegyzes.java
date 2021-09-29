dc332a040c988050f35b3bc131e972ca0ae92c6c
Szalai Szabolcs <sz.szalai16@gmail.com>
2021-09-29 14:53:16 +0200

    Privaze adattagok, metódusok létrehozva

--- src/hu/petrik/Bejegyzes.java
+++ src/hu/petrik/Bejegyzes.java
@@ -1,4 +1,48 @@
 package hu.petrik;
 
+import java.time.LocalDateTime;
+
 public class Bejegyzes {
+    private String szerzo;
+    private String tartalom;
+    private int likeok;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+
+    public Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+        this.likeok = 0;
+        this.letrejott = LocalDateTime.now();
+        this.szerkesztve = LocalDateTime.now();
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
+    }
+
+    public void like() {
+
+    }
 }
